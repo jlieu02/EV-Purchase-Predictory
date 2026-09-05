f4a94b5bc949de78020e0f3eaab446c7755f18f5
jlieu02 <jayxlieu@gmail.com>
2026-09-05 15:44:53 -0700

    Filter dataset to contain only those who will buy EVS

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from load import load_data
+import pandas as pd
 
 train, test, sample = load_data()
+
+#exploratory data analysis
 print(train.shape, test.shape)
 print(train.head())
+
+will_buy = train[train["Will_Buy_EV"] == "Yes"]
+print(will_buy.head())
