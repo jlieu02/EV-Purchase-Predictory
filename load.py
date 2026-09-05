a625eff017361c735320fe6cefd967b2968c967d
Eliu-09 <ethanydliu@gmail.com>
2026-09-05 15:16:36 -0700

    fixed load

--- load.py
+++ load.py
@@ -9,7 +9,6 @@ import pandas as pd
 
 COMPETITION = "playground-series-s6e9"
 
-
 def load_data():
     path = kagglehub.competition_download(COMPETITION)
 
@@ -18,10 +17,3 @@ def load_data():
     sample = pd.read_csv(os.path.join(path, "sample_submission.csv"))
 
     return train, test, sample
-
-
-if __name__ == "__main__":
-    train, test, sample = load_data()
-    print("train:", train.shape)
-    print("test:", test.shape)
-    print(train.head())
